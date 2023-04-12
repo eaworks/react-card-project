fef233d7598e599dd1bbced38324fe0a1d6b63a5
Ersin Aydın <ersinaydinworks@gmail.com>
2023-04-12 10:18:35 +0300

    grid yapisi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { Container, Grid } from '@mui/material';
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Course from './Course';
+import Angular from './images/angular.jpg';
+import Bootstrap from './images/bootstrap.jpg';
+import Csharp from './images/csharp.jpg';
+import KompleWeb from './images/completeweb.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar>
+        <Toolbar sx={{ fontSize: 30 }}>Kart Projesi</Toolbar>
+      </AppBar>
+      <Container maxWidth="lg">
+        <Grid container spacing={2} sx={{ marginTop: 10 }}>
+          <Grid item xs={12} md={6} lg={3}>
+            <Course image={Angular} title="Angular" description="Lorem commodo commodo aliqua ipsum aliqua veniam consectetur enim excepteur adipisicing incididunt duis voluptate sit. Ex velit ea irure cupidatat. Duis deserunt nulla deserunt sunt pariatur ullamco nulla non." />
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Course image={Bootstrap} title="Bootstrap" description="Lorem commodo commodo aliqua ipsum aliqua veniam consectetur enim excepteur adipisicing incididunt duis voluptate sit. Ex velit ea irure cupidatat. Duis deserunt nulla deserunt sunt pariatur ullamco nulla non." />
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Course image={Csharp} title="Csharp" description="Lorem commodo commodo aliqua ipsum aliqua veniam consectetur enim excepteur adipisicing incididunt duis voluptate sit. Ex velit ea irure cupidatat. Duis deserunt nulla deserunt sunt pariatur ullamco nulla non." />
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <Course image={KompleWeb} title="KompleWeb" description="Lorem commodo commodo aliqua ipsum aliqua veniam consectetur enim excepteur adipisicing incididunt duis voluptate sit. Ex velit ea irure cupidatat. Duis deserunt nulla deserunt sunt pariatur ullamco nulla non." />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
---
+++ src/Course.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Course({ image, title, description }) {
+    return (
+        <div>
+            <img src={image} alt='' />
+            <p>{title}</p>
+            <p>{description}</p>
+        </div>
+    )
+}
+
+export default Course
